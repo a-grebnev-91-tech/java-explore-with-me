3db9b7c356f879bbf9c352d2cab73f9404957251
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2022-11-06 13:53:31 +0300

    feat: add containerization for tg bot

--- ewm-telegram-bot/src/main/java/ru/practicum/exception/NotFoundException.java
+++ ewm-telegram-bot/src/main/java/ru/practicum/exception/NotFoundException.java
@@ -1,8 +1,13 @@
 package ru.practicum.exception;
 
-//TODO add handler
+import lombok.Getter;
+
+@Getter
 public class NotFoundException extends RuntimeException {
+    private final String reason;
+
     public NotFoundException(String message, String reason) {
         super(message);
+        this.reason = reason;
     }
 }
