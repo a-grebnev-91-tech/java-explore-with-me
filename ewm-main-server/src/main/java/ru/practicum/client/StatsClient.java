329389ea8f461a55ff47cbccecac3671417d073c
a-grebnev-91-tech <a.grebnev.91.tech@yandex.ru>
2022-11-07 15:51:25 +0300

    refact: code style

--- ewm-main-server/src/main/java/ru/practicum/client/StatsClient.java
+++ ewm-main-server/src/main/java/ru/practicum/client/StatsClient.java
@@ -1,6 +1,5 @@
 package ru.practicum.client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
